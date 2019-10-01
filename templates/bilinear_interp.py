79ea331ff926b3b33b40f37b900e160e38285d55
Nam <lkunelk@gmail.com>
2019-10-01 15:04:16 -0400

    finished implementing bilinear interpolation, fixed issue where I should have rounded instead of converting to int

--- templates/bilinear_interp.py
+++ templates/bilinear_interp.py
@@ -23,11 +23,28 @@ def bilinear_interp(I, pt):
     --------
     b  - Interpolated brightness or intensity value (whole number >= 0).
     """
-    #--- FILL ME IN ---
-
     if pt.shape != (2, 1):
         raise ValueError('Point size is incorrect.')
 
-    #------------------
+    # Find 4 nearest pixels
+    x, y = pt[0,0], pt[1,0]
+    pt1 = np.floor([x, y]).astype(np.int)
+    pt2 = np.array([np.ceil(x),  np.floor(y)]).astype(np.int)
+    pt3 = np.array([ np.floor(x), np.ceil(y)]).astype(np.int)
+    pt4 = np.ceil( [x, y]).astype(np.int)
+    
+    # Interpolate in x direction
+    vx1 = (1.0 - (x - pt1[0])) * I[pt1[1], pt1[0]] +\
+                 (x - pt1[0])  * I[pt2[1], pt2[0]]
+    vx2 = (1.0 - (x - pt1[0])) * I[pt3[1], pt3[0]] +\
+                 (x - pt1[0])  * I[pt4[1], pt4[0]]
+    
+    # Interpolate in y direction
+    b = (1.0 - (y - pt1[1])) * vx1 +\
+               (y - pt1[1])  * vx2
+    return np.round(b)
 
-    return b
+if __name__ == '__main__':
+    I = np.array([[10,20],[30,40]])
+    pt = np.array([[-1, 0]]).T
+    print(bilinear_interp(I, pt))
