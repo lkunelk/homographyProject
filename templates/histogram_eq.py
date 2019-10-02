f923041b387dccb3c2609ec80450a60a3dc6a036
Nam <lkunelk@gmail.com>
2019-10-02 16:24:12 -0400

    implemented histogram algorithm

--- templates/histogram_eq.py
+++ templates/histogram_eq.py
@@ -17,11 +17,32 @@ def histogram_eq(I):
     J  - Contrast-enhanced greyscale intensity image, 8-bit np.array (i.e., uint8).
     """
     #--- FILL ME IN ---
-
+    
+    k = 2**8
+    height, width = I.shape
+    
     # Verify I is grayscale.
     if I.dtype != np.uint8:
         raise ValueError('Incorrect image format!')
-
-    #------------------
-
+    
+    # compute histogram
+    h = [0 for i in range(k)]
+    for row in I:
+        for val in row:
+            h[val] += 1
+    
+    # compute pdf
+    for i in range(1, k):
+        h[i] += h[i-1]
+    
+    J = np.zeros(I.shape).astype(np.uint8)
+    for row in range(height):
+        for col in range(width):
+            J[row, col] = round((h[I[row, col]] - h[0]) * (k-1) / (width * height - h[0]))
+    
     return J
+
+if __name__ == '__main__':
+    A = np.array([[1,2],[3,4]]).astype(np.uint8)
+    J = histogram_eq(A)
+    print(J)
